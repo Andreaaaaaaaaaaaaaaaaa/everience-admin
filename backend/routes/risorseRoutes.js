da8ab93d7beb84f507777a317fe2cdc9fd468a0b
Andrea <andrea.grillo@everience.com>
2026-06-09 17:35:34 +0200

    feat: update risorseRoutes to enable authentication middleware and modify index.html for async loading of Google Maps script

--- backend/routes/risorseRoutes.js
+++ backend/routes/risorseRoutes.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const risorseController = require('../controllers/risorseController');
 const authMiddleware = require('../middleware/auth');
-//router.get('/', authMiddleware, risorseController.findAll);
+
+//commentato per testare router.get('/', authMiddleware, risorseController.findAll);
+/*riga sostitutiva */router.get('/', risorseController.findAll);
 
 module.exports = router;
